5154920093565beff7109719b3979bef0caded79
Tyom9730 <tyom.asatryan97@gmail.com>
2023-06-02 01:01:58 +0400

    changes

--- src/app/components/basestocks-introduction-overview/basestocks-introduction-overview.component.ts
+++ src/app/components/basestocks-introduction-overview/basestocks-introduction-overview.component.ts
@@ -4,17 +4,14 @@ import { LandingServiceService } from 'src/app/services/landing-service.service'
 @Component({
   selector: 'app-basestocks-introduction-overview',
   templateUrl: './basestocks-introduction-overview.component.html',
-  styleUrls: ['./basestocks-introduction-overview.component.scss']
+  styleUrls: ['./basestocks-introduction-overview.component.scss'],
 })
 export class BasestocksIntroductionOverviewComponent implements OnInit {
-
-  public basestockOverviewData: any[] = []
-
+  public basestockOverviewData: any[] = [];
 
   constructor(private service: LandingServiceService) {}
 
   ngOnInit(): void {
-      this.basestockOverviewData = this.service.basestockOverviewData
-      console.log('basestockOverviewData',this.basestockOverviewData)
+    this.basestockOverviewData = this.service.basestockOverviewData;
   }
 }
--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -7,17 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.scss'],
 })
 export class FaqComponent implements OnInit {
-
-  public faqData: any[] = []
-
+  public faqData: any[] = [];
+  public navigationFaq: any[] = [];
+  public scopeFaq: any[] = [];
+  public aboutFaq: any[] = [];
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http
-    .get('../../../assets/json/faqs_data.json')
-    .subscribe((data: any) => {
-      this.faqData = data
-      console.log('this.faqData', this.faqData)
-    });
+      .get('../../../assets/json/faqs_data.json')
+      .subscribe((data: any) => {
+        this.faqData = data;
+        this.navigationFaq = data.filter((item) => item.section === 'one');
+        this.scopeFaq = data.filter((item) => item.section === 'two');
+        this.aboutFaq = data.filter((item) => item.section === 'three');
+        console.log('this.faqData', this.faqData);
+        console.log('this.navigationFaq', this.navigationFaq);
+      });
   }
 }
